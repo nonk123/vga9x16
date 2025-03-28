047d3a72879edf2ae8312d89ec708d97c16f4165
nonk123 <me@nonk.dev>
2025-03-28 09:51:40 +0300

    Another caching hack

--- src/responders.rs
+++ src/responders.rs
@@ -10,7 +10,8 @@ pub struct AsciiBlob<D>(pub D, pub &'static str);
 impl<'r, 'o: 'r, D: Responder<'r, 'o>> Responder<'r, 'o> for AsciiBlob<D> {
     fn respond_to(self, req: &'r Request<'_>) -> response::Result<'o> {
         let mut response = self.0.respond_to(req)?;
-        response.set_header(Header::new("Cache-Control", "public, no-store"));
+        let caching = "public, no-store, max-age=1, s-maxage=1";
+        response.set_header(Header::new("Cache-Control", caching));
         response.set_header(Header::new("Content-Type", self.1));
         Ok(response)
     }
